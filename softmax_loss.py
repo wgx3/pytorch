47e21fbd701313c578bfe38fe5f9a02f4170c274
wgx <616001452@qq.com>
2018-08-20 17:09:10 +0800

    forth commit

--- softmax_loss.py
+++ softmax_loss.py
@@ -7,3 +7,13 @@ from torchvision import datasets,transforms
 from torch.autograd import Variable
 
 logsm = nn.LogSoftmax()
+loss = nn.NLLLoss()
+
+input = Variable(torch.randn(3,5),requires_grad=True)
+logsm_out = logsm(input)
+
+target = Variable(torch.LongTensor([1,0,4]))
+l = loss((logsm_out), target)
+l.backward()
+
+print(input.size(), target.size(), l.size())
--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-import numpy as np 
-print(np.nan)
+# import numpy as np 
+# a = np.zeros((4,4))
+# print(a)
+# x_data = a[:, 0:-1]
+# y_data = a[:, [-1]]
+# print(x_data)
+# print(y_data)
+
+import torch
+print(torch.__version__)
